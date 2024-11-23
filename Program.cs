975f30f0b5a9b268f749c8de408b0122c9c651e0
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2024-11-23 14:01:01 -0300

    Get operation type in terminal
    Create a method called ChosenOperation() to get the operation desired by the user

--- Program.cs
+++ Program.cs
@@ -8,6 +8,14 @@ struct EReturnValueInput
     public EReturnValueInput(float fisrtValue, float secondValue) => (FisrtValue, SecondValue) = (fisrtValue, secondValue);
 }
 
+enum EArithmeticOperations
+{
+    Sum = 0,
+    Substract = 1,
+    Multiply = 2,
+    Divide = 3,
+}
+
 namespace SimpleCalculator
 {
     internal static class Program
@@ -17,6 +25,7 @@ namespace SimpleCalculator
             Console.Clear();
             WelcomeMessage();
             var values = GetValues();
+            var operation = ChosenOperation();
         }
 
         private static void WelcomeMessage()
@@ -33,6 +42,30 @@ namespace SimpleCalculator
             var secondValue = float.Parse(Console.ReadLine());
             
             return new EReturnValueInput(firstValue, secondValue);
+        }
+
+        private static EArithmeticOperations ChosenOperation()
+        {
+            Console.WriteLine("Enter the operation: ");
+            Console.WriteLine("0 - Sum");
+            Console.WriteLine("1 - Substract");
+            Console.WriteLine("2 - Multiply");
+            Console.WriteLine("3 - Divide");
+            var operation = float.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case 0:
+                    return EArithmeticOperations.Sum;
+                case 1:
+                    return EArithmeticOperations.Substract;
+                case 2: 
+                    return EArithmeticOperations.Multiply;
+                case 3: 
+                    return EArithmeticOperations.Divide;
+                default: 
+                    return EArithmeticOperations.Sum;
+            }
         }
     }    
 }
