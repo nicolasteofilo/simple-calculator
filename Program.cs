15ae2734bfa86b6330e5c7d6916ac355b97acc5f
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2024-11-23 12:58:48 -0300

    Get numeric values from terminal with GetValues()
    Create a method called GetValues() to get the values from terminal with
    Console.ReadLine()

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 ﻿using SimpleCalculator;
 
+struct EReturnValueInput
+{
+    public float FisrtValue;
+    public float SecondValue;
+    
+    public EReturnValueInput(float fisrtValue, float secondValue) => (FisrtValue, SecondValue) = (fisrtValue, secondValue);
+}
+
 namespace SimpleCalculator
 {
     internal static class Program
@@ -8,11 +16,23 @@ namespace SimpleCalculator
         {
             Console.Clear();
             WelcomeMessage();
+            var values = GetValues();
         }
 
         private static void WelcomeMessage()
         {
             Console.WriteLine("***** Fun with Calculator *****");
+        }
+
+        private static EReturnValueInput GetValues()
+        {
+            Console.Write("Enter the first number: ");
+            var firstValue = float.Parse(Console.ReadLine()) ;
+            
+            Console.Write("Enter the second number: ");
+            var secondValue = float.Parse(Console.ReadLine());
+            
+            return new EReturnValueInput(firstValue, secondValue);
         }
     }    
 }
