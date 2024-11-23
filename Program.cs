1a24e66ed05a49bbc17d4565c1142b9daa0458cc
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2024-11-23 12:14:34 -0300

    Initialize project with WelcomeMessage()

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using SimpleCalculator;
+
+namespace SimpleCalculator
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Clear();
+            WelcomeMessage();
+        }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("***** Fun with Calculator *****");
+        }
+    }    
+}
+
